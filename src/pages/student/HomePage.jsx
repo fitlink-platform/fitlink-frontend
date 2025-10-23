5083ae215e1640216392c1102e4faa8d4a1e822f
Lê Quang Huy <HuyLQDE170077@fpt.edu.vn>
2025-10-24 00:39:16 +0700

    complete bmi ui

--- src/pages/student/HomePage.jsx
+++ src/pages/student/HomePage.jsx
@@ -1,11 +1,12 @@
 // src/pages/CustomerPage.jsx
 import HeroCarousel from "~/components/HeroCarousel";
 import MainLayout from "~/layouts/MainLayout";
-
+import BMIFloatingButton from "~/components/student/BMIFloatingButton";
 export default function CustomerPage() {
   return (
     <MainLayout>
       <HeroCarousel />
+      <BMIFloatingButton />
     </MainLayout>
   );
 }
